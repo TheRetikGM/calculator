4e24b385c376486a291eaf54a2b945205a338af9
TheRetkGM <theretikgm@gmail.com>
2018-12-12 20:20:36 +0000

    cfybhdxkhv

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include "numpad.h"
+#define CGRN  "\x1B[32m"
+#define CNRM  "\x1B[0m"
 
 int main()
 {
 	char buf[1024];
 
 	numpad(6, 17);
-	printf("Output from numpad: \"%s\"\n", outstring);
+	printf("Output from numpad: %s\"%s\"%s\n", CGRN, outstring, CNRM);
 	sprintf(buf, "echo %s | ./dscc", outstring);
 	system(buf);
 	return 0;
